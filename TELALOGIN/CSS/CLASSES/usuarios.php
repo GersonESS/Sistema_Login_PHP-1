6076a409f2d381d8e19a7c703876854c21a8d752
Gerson Santos <gersones@outlook.com>
2023-10-30 01:06:33 -0300

    a

---
+++ TELALOGIN/CSS/CLASSES/usuarios.php
@@ -0,0 +1,67 @@
+<?php
+Class Usuario
+{
+    private $pdo;
+    public $msgErro = "";
+
+    public function conectar($nome, $host, $usuario ,$senha)
+    {
+        global $pdo;
+        global $msgErro;
+        try
+        { 
+            $pdo = new pdo("mysql:dbname=".$nome.";host=".$host,$usuario,$senha);
+
+        }catch(PDOException $e){
+            $msgErro = $e->getMessage();
+        }
+    }
+    public function cadastrar($nome, $telefone, $email, $senha)
+    {
+        global $pdo;
+        //verificar se  ja existe o email cadastrado
+        $sql = $pdo->prepare("SELECT id_usuario FROM usuarios WHERE email = :e");
+        $sql->bindValue(":e",$email);
+        $sql->execute();
+        if($sql->rowCount() > 0)
+        {
+            return false; //ja esta cadastrada
+        } 
+        else
+        {
+            $sql = $pdo->prepare("INSERT INTO usuarios (nome, telefone, email, senha)
+            VALUES (:n, :t, :e, :s)");
+             $sql->bindValue(":n",$nome);
+             $sql->bindValue(":t",$telefone);
+             $sql->bindValue(":e",$email);
+             $sql->bindValue(":s",$senha);
+             $sql->execute();
+             return true;
+        }
+        //caso não, Cadstrar
+    }
+    
+    public function logar($nome, $telefone, $email, $senha)
+    {
+        global $pdo;
+        $sql = $pdo->prepare("SELECT id_usuario FROM usuarios WHERE email = :e AND senha =:s");
+        $sql->bindValue(":e",$email);
+        $sql->bindValue(":s",$senha);
+        $sql->execute();
+        if($sql->rowCount() > 0)
+        {
+            //entrar no sistema
+            $dado = 
+parei 4:18 parte 4/5
+
+
+        } 
+        else
+        {
+
+
+             return true;
+
+    }
+}
+?>
